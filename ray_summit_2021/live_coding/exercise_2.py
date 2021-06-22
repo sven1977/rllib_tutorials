e02f2cdc126047a97f33d164820d492f949c295a
sven1977 <svenmika1977@gmail.com>
2021-06-22 13:48:23 +0200

    wip.

--- ray_summit_2021/live_coding/exercise_2.py
+++ ray_summit_2021/live_coding/exercise_2.py
@@ -1,21 +1,21 @@
 # Solution to Exercise #2:
 
-# Update our config and set it up for 2x tune grid-searches (leading to 4 parallel trials in total).
-config.update({
-    "lr": tune.grid_search([0.0001, 0.0005]),
-    "train_batch_size": tune.grid_search([2000, 3000]),
-    "num_envs_per_worker": 5,
-    # Change our model to be simpler.
-    "model": {
-        "fcnet_hiddens": [128, 128],
-    },
-})
+# Run for longer this time (not just 2 iterations) and try to reach 40.0 reward (sum of both agents).
+stop = {
+    "training_iteration": 200,
+    "episode_reward_mean": 50.0,
+}
 
-# Run the experiment.
-analysis = tune.run(
-    "PPO",
-    config=config,
-    stop={"episode_reward_mean": 15.0, "training_iteration": 100},
-    checkpoint_freq=10,
-    checkpoint_at_end=True,
-)
+# tune_config.update({
+# ???
+# })
+
+# analysis = tune.run(...)
+
+tune_config["lr"] = 0.00005
+tune_config["train_batch_size"] = 4000
+tune_config["num_envs_per_worker"] = 10
+tune_config["num_workers"] = 5
+tune_config["model"] = {"fcnet_hiddens": [512, 512]}
+
+analysis = tune.run("PPO", config=tune_config, stop=stop, checkpoint_at_end=True, checkpoint_freq=10)
